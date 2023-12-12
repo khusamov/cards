7a89fac33723b97ad5be672824000c2b7874446e
Khusamov Sukhrob <khusamov@yandex.ru>
2023-12-12 23:13:00 +0300

    Добавлена поддержка TypeScript-декораторов

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		react({
+			babel: {
+				plugins: [
+					[
+						// Добавлена поддержка TypeScript-декораторов.
+						// https://babeljs.io/docs/babel-plugin-proposal-decorators
+						// https://stackoverflow.com/questions/66395054/how-do-i-enable-babel-plugin-proposal-decorators-with-vite
+						'@babel/plugin-proposal-decorators',
+						{
+							version: '2022-03'
+						}
+					]
+				]
+			}
+		})
+	]
 })

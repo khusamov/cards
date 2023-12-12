4b99b4ed37ead96aa17f312f607d97a96c620d0e
Khusamov Sukhrob <khusamov@yandex.ru>
2023-12-12 16:00:13 +0300

    Добавлена библиотека @mui/joy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import {Button, CssVarsProvider, Sheet} from '@mui/joy'
 import './App.scss'
 
-export const App = () => (
-	<div>
-		dss
-	</div>
+export const App = (
+	() => (
+		<CssVarsProvider>
+			<Sheet variant={'outlined'}>
+				<Button>Тренировочные карточки</Button>
+			</Sheet>
+		</CssVarsProvider>
+	)
 )
